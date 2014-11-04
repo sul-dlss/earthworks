823e79856ed59dfb19e6a6519085fb1b9644bc24
Jack Reed <pjreed@stanford.edu>
2014-11-04 10:23:58 -0800

    initial skeleton geoblacklight app

---
+++ README.md
@@ -0,0 +1,6 @@
+## EarthWorks
+
+Geospatial discovery application for Stanford University Libraries. Built using [GeoBlacklight](https://github.com/geoblacklight/geoblacklight)
+
+### Running application
+`$ rails s`
---
+++ app/models/solr_document.rb
@@ -0,0 +1,23 @@
+# -*- encoding : utf-8 -*-
+class SolrDocument 
+
+  include Blacklight::Solr::Document
+  include Geoblacklight::SolrDocument
+
+  # self.unique_key = 'id'
+  self.unique_key = 'layer_slug_s'
+
+  # Email uses the semantic field mappings below to generate the body of an email.
+  SolrDocument.use_extension(Blacklight::Solr::Document::Email )
+
+  # SMS uses the semantic field mappings below to generate the body of an SMS email.
+  SolrDocument.use_extension(Blacklight::Solr::Document::Sms )
+
+  # DublinCore uses the semantic field mappings below to assemble an OAI-compliant Dublin Core document
+  # Semantic mappings of solr stored fields. Fields may be multi or
+  # single valued. See Blacklight::Solr::Document::ExtendableClassMethods#field_semantics
+  # and Blacklight::Solr::Document#to_semantic_values
+  # Recommendation: Use field names from Dublin Core
+  use_extension(Blacklight::Solr::Document::DublinCore)
+
+end
