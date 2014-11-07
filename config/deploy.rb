bf1a8bc5f44482f46a941ab7420e8eb21a2d2527
Jack Reed <pjreed@stanford.edu>
2014-11-07 13:15:05 -0500

    add dev deploy

---
+++ config/deploy.rb
@@ -0,0 +1,62 @@
+# config valid only for Capistrano 3.1
+lock '3.2.1'
+
+set :application, 'earthworks'
+set :repo_url, 'https://github.com/sul-dlss/earthworks.git'
+set :deploy_host, ask("Server", 'e.g. hostname with no ".stanford.edu" or server node designator')
+ask :user, proc { `whoami`.chomp }.call
+
+# Default branch is :master
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+
+server "#{fetch(:deploy_host)}.stanford.edu", user: fetch(:user), roles: %w{web db app}
+
+
+Capistrano::OneTimeKey.generate_one_time_key!
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+set :log_level, :info
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+set :linked_files, %w{config/database.yml config/solr.yml}
+
+# Default value for linked_dirs is []
+set :linked_dirs, %w{bin config/settings log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+end
---
+++ config/deploy/development.rb
@@ -0,0 +1,6 @@
+set :home_directory, "/opt/app/#{fetch(:user)}"
+set :deploy_to, "#{fetch(:home_directory)}/#{fetch(:application)}"
+
+set :bundle_without, %w{sqlite production test}.join(' ')
+
+set :rails_env, "development"
