051e57ddc16abea4ed0e972587ec3c249caf0263
Jack Reed <pjreed@stanford.edu>
2014-11-12 14:20:56 -0800

    proposed changes for deployment

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,6 +1,7 @@
-set :home_directory, "/opt/app/#{fetch(:user)}"
-set :deploy_to, "#{fetch(:home_directory)}/#{fetch(:application)}"
-
+set :deploy_host, ask("Server", 'e.g. server.stanford.edu')
 set :bundle_without, %w{sqlite production test}.join(' ')
 
+server fetch(:deploy_host), user: fetch(:user), roles: %w{web db app}
+
+Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, "development"
