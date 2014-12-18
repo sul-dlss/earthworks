918f308a53fae0501d4d2bc1754cef7215727fc0
Darren Hardy <drh@stanford.edu>
2014-12-17 16:14:35 -0800

    add default from address to enable email features

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_mailer.default_options = {
+    :from => 'no-reply@earthworks.stanford.edu'
+  }
 end
