4c24f2442913815cd1c21bf86d37e77d272b2d4f
Darren Hardy <drh@stanford.edu>
2014-12-18 14:44:48 -0800

    send param :url to know where to go after login. fixes #99

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,7 +1,10 @@
 class LoginController < ApplicationController
 
   def login
-    redirect_to params[:referrer] || :back
+    # devise will handle the redirection to WebAuth and WebAuth will use that referrer
+    # to send us back to whence we came, but since the referrer will be our login auth page,
+    # we send a parameter so we can take the user back to their starting page
+    redirect_to params[:url] || '/'
   end
 
 end
