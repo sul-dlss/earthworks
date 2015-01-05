7c5628c758f567b9101573f3b39f51cdbbd5731b
Darren Hardy <drh@stanford.edu>
2015-01-05 15:13:37 -0800

    adds the ability to send in only the score field for update

--- tools/geomonitor/update.rb
+++ tools/geomonitor/update.rb
@@ -10,10 +10,12 @@ require 'nokogiri'
 if ARGV.size < 2
   puts 'Usage: update.rb http://localhost:8080/solr/my-collection file1.json [file2.json...]'
   puts 'where JSON data has [["slug1", 0.1], ["slug2", 0.9] ...]'
+  puts 'supports environment variable UPDATE_BY_FIELD=true for field-level atomic updates in Solr 4'
   exit -1
 end
 
-STOP_ON_ERROR = false
+STOP_ON_ERROR = ENV['STOP_ON_ERROR'] == 'true' || false
+UPDATE_BY_FIELD = ENV['UPDATE_BY_FIELD'] == 'true' || false
 SCORE_FIELD = 'layer_availability_score_f'
 
 solr = RSolr.connect :url => ARGV.delete_at(0) 
@@ -25,16 +27,30 @@ ARGV.each do |fn|
     if fn =~ /.json$/
       data = JSON.parse(File.open(fn, 'rb').read)
       data.each do |slug, score|
-        puts "Processing #{slug} #{score}"
+        puts "Processing #{n} #{slug} #{score}"
         q = "layer_slug_s:\"#{slug}\""
         res = solr.get 'select', :params => { :q=>q, :start=>0, :rows=>1 }
-        doc = res['response']['docs'][0]
+        doc = res['response']['docs'].first # assumes that we got a single hit using rows=1
         unless doc.nil?
           doc.delete('_version_')
           doc.delete('timestamp')
           doc.delete('score')
           doc[SCORE_FIELD] = score
-          solr.add doc
+          
+          unless UPDATE_BY_FIELD
+            solr.add doc
+          else
+            # Build raw XML document so that we can do partial update of the SCORE_FIELD
+            # There are numerous caveats here: https://wiki.apache.org/solr/Atomic_Updates
+            doc = {
+              'uuid' => doc['uuid'],
+              SCORE_FIELD => doc[SCORE_FIELD]
+            }
+            add_xml = solr.xml.add(doc) do |solr_xml_doc|
+              solr_xml_doc.field_by_name(SCORE_FIELD).attrs[:update] = 'set'
+            end
+            solr.update :data => add_xml
+          end
           
           n = n + 1
           if n % 100 == 0
@@ -42,7 +58,7 @@ ARGV.each do |fn|
             solr.commit
           end
         else
-          puts "WARNING: Missing #{slug}"
+          puts "WARNING: Missing #{slug} in Solr index"
         end
       end
     else
