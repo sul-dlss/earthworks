657541c1ab26f2e34ab6908f3daf78798afaa887
Darren Hardy <drh@stanford.edu>
2015-02-03 16:00:11 -0800

    rename stanford_rights_metadata_ss to stanford_rights_metadata_s

--- app/models/concerns/rights_metadata_concern.rb
+++ app/models/concerns/rights_metadata_concern.rb
@@ -2,6 +2,6 @@ module RightsMetadataConcern
   extend Geoblacklight::SolrDocument
 
   def rights_metadata
-    RightsMetadata.new(get(:stanford_rights_metadata_ss))
+    RightsMetadata.new(get(:stanford_rights_metadata_s))
   end
 end
