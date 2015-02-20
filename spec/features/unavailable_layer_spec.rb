4627baadc17554cb8a8bd42c5837a319eee6901f
Jack Reed <pjreed@stanford.edu>
2015-02-20 11:44:38 -0800

    adds in message if no downloads are available

--- spec/features/unavailable_layer_spec.rb
+++ spec/features/unavailable_layer_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
 feature 'Unavailable layer' do
-  scenario 'hides and shows appropriate messages' do
+  scenario 'hides and shows appropriate messages for geomonitored unavailable' do
     visit catalog_path 'harvard-ntadcd106'
     within '.unavailable-warning' do
       expect(page).to have_content 'Harvard'
+      expect(page).to have_content 'unavailable to preview and download'
     end
     expect(page).to_not have_content 'Download Shapefile'
+  end
+  scenario 'with no downloads shows no download message' do
+    visit catalog_path 'princeton-02870w62c'
+    within '.unavailable-warning' do
+      expect(page).to have_content 'Princeton'
+      expect(page).to have_content 'unavailable to download'
+    end
   end
 end
