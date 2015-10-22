056a27218907a66137d4dd67d42a90d3685c5af3
Jack Reed <pjreed@stanford.edu>
2015-10-22 08:58:27 -0700

    Create a Earthworks::SearchBuilder to make spatial search results more relevant

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module Earthworks
     require 'suggest/response'
     require 'suggest/search_helper'
     require 'suggest'
+    require 'search_builder'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
