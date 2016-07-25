7ad23ba1e4b73e7320066dec6e0417f108d513a4
Jack Reed <phillipjreed@gmail.com>
2016-07-25 09:06:07 -0400

    update routes to use new BL6 and GeoBL 1 style

--- spec/features/saved_searches_spec.rb
+++ spec/features/saved_searches_spec.rb
@@ -6,7 +6,7 @@ feature 'Saved searches' do
   scenario 'test' do
     visit catalog_index_path q: '*'
     click_button 'Save this search'
-    visit saved_searches_path
+    visit blacklight.saved_searches_path
     expect(page).to have_css 'td', count: 2 #Saved searches are stored in a table
   end
 end
