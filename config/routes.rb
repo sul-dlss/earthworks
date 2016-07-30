0eaaa90d13a55620c283d61d8377d6d386934950
Jack Reed <phillipjreed@gmail.com>
2016-07-30 12:23:22 -0400

    update show route as catalog

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   get 'feedback' => 'feedback_forms#new'
 
   concern :gbl_exportable, Geoblacklight::Routes::Exportable.new
-  resources :solr_documents, only: [:show], controller: 'catalog' do
+  resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
     concerns :gbl_exportable
   end
 
