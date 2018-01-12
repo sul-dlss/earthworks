4e8a3716bed55c7feeda358fe2f3a560cb621074
Jack Reed <phillipjreed@gmail.com>
2018-01-12 10:28:49 -0700

    convert indexer update methods to class

--- app/models/concerns/status_extension.rb
+++ app/models/concerns/status_extension.rb
@@ -9,14 +9,6 @@ module StatusExtension
       layer_availability_score_f: { set: layer.availability_score },
       layer_slug_s: layer.slug
     }]
-    Blacklight.default_index.connection.update(
-      params: { commitWithin: commit_within, overwrite: true },
-      data: data.to_json,
-      headers: { 'Content-Type' => 'application/json' }
-    )
-  end
-
-  def commit_within
-    ENV['SOLR_COMMIT_WITHIN'] || Settings.SOLR_COMMIT_WITHIN || 5000
+    Indexer.new.solr_update(data)
   end
 end
---
+++ app/models/indexer.rb
@@ -0,0 +1,16 @@
+##
+# A simple class for sending updates to Solr
+class Indexer
+
+  def solr_update(data)
+    Blacklight.default_index.connection.update(
+      params: { commitWithin: commit_within, overwrite: true },
+      data: data.to_json,
+      headers: { 'Content-Type' => 'application/json' }
+    )
+  end
+
+  def commit_within
+    ENV['SOLR_COMMIT_WITHIN'] || Settings.SOLR_COMMIT_WITHIN || 5000
+  end
+end
