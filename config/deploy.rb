bc71c11772456ca23f7a745969cddfd12b06edea
Chris Beer <cabeer@stanford.edu>
2018-05-10 13:16:11 -0700

    Add NewRelic

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,7 @@ set :log_level, :info
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/honeybadger.yml config/secrets.yml config/blacklight.yml public/robots.txt}
+set :linked_files, %w{config/database.yml config/honeybadger.yml config/secrets.yml config/blacklight.yml public/robots.txt config/newrelic.yml }
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{config/settings log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/sitemaps}
