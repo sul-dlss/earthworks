858024e03329215cb7edd8bf22edf467b035cc7b
Chris Beer <chris@cbeer.info>
2019-05-16 11:53:39 -0700

    Swap chromedriver-helper for webdrivers (#512)
    * Swap chromedriver-helper for webdrivers
    
    * Update rubygems to work around a malformed version error with webdrivers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,15 +7,7 @@ require 'rspec/rails'
 require 'devise'
 require 'factory_bot'
 
-Capybara.javascript_driver = :headless_chrome
-
-Capybara.register_driver :headless_chrome do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: %w[headless disable-gpu no-sandbox] }
-  )
-
-  Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
-end
+Capybara.javascript_driver = :selenium_chrome_headless
 
 Capybara.default_max_wait_time = 10
 # Add additional requires below this line. Rails is not loaded until this point!
