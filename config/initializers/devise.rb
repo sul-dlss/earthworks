40bc6ddc8c743d1bb90f37acda87aec9fc6444e2
Chris Beer <cabeer@stanford.edu>
2020-03-18 07:14:55 -0700

    Guard against an empty REMOTE_USER header

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,7 +1,7 @@
 require 'devise_remote_user'
 
 DeviseRemoteUser.configure do |config|
-  config.env_key = lambda { |env| env['REMOTE_USER'] }
+  config.env_key = lambda { |env| env['REMOTE_USER'].presence }
   config.auto_create = true
   config.auto_update = true
   config.attribute_map = { webauth_groups: 'WEBAUTH_LDAPPRIVGROUP' }
