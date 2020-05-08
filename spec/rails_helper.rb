58180a4cb4fda09a8afc29d065221064a5e4074d
Jack Reed <phillipjreed@gmail.com>
2020-05-08 11:49:14 -0600

    Add user stub supporting

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,6 +31,11 @@ Capybara.default_max_wait_time = 10
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
