2ebd670172bb75e5aaac3f14520c050daa3a6f6d
Chris Beer <cabeer@stanford.edu>
2020-06-26 08:42:47 -0700

    Update vector data with styling appropriate to the geom type

--- scripts/geoserver/set_wms_default_style.rb
+++ scripts/geoserver/set_wms_default_style.rb
@@ -12,6 +12,14 @@ raise('GEOSERVER_URL not provided') unless geoserver_url
 raise('GEOSERVER_USER not provided') unless ENV['GEOSERVER_USER']
 raise('GEOSERVER_PASS not provided') unless ENV['GEOSERVER_PASS']
 
+solr_data = Faraday.get('https://sul-solr-a.stanford.edu/solr/kurma-earthworks-v1-prod/select?fl=layer_slug_s,layer_geom_type_s&q=dct_provenance_s:Stanford&fq=-layer_geom_type_s:Raster&fq=-layer_geom_type_s:Image&rows=20000&wt=csv&csv.header=false')
+expected_styles = solr_data.body.split("\n").each_with_object({}) do |row, hash|
+  slug, geom = row.split(',', 2)
+  _, druid = slug.split('-', 2)
+
+  hash[druid] = geom.downcase
+end
+
 puts "Updating WMS default style for all vector layers on #{geoserver_url}"
 
 conn = Faraday.new(url: geoserver_url) do |faraday|
@@ -28,34 +36,38 @@ layer_names = Nokogiri::XML(layers_response.body).xpath('//layer/name').map(&:te
 puts "Found #{layer_names.length} layers"
 Nokogiri::XML(layers_response.body).xpath('//layer').each do |layer|
   name = layer.xpath('name').text
+  druid = name.strip.sub('druid:', '')
+
   layer_rest = layer.xpath('atom:link', atom: 'http://www.w3.org/2005/Atom').attribute('href').to_s
 
   layer_response = conn.get do |req|
     req.url layer_rest.gsub(geoserver_url, '')
   end.body
   parsed_response = Nokogiri::XML(layer_response)
-  if parsed_response.xpath('//type').text == 'VECTOR'
-    print "#{name} is a vector"
-    style = parsed_response.xpath('//defaultStyle').text.strip
-    unless style.length > 0
-      print ' no defaultStyle is set setting to generic '
-      generic_style =
-        <<~XML
-          <layer>
-            <defaultStyle>
-              <name>generic</name>
-            </defaultStyle>
-          </layer>
-        XML
-      update = conn.put do |req|
-        req.url layer_rest.gsub(geoserver_url, '')
-        req.headers['Content-Type'] = 'text/xml'
-        req.body = generic_style
-      end
-      puts update.status
+  next unless parsed_response.xpath('//type').text == 'VECTOR'
 
-    else
-      puts " defaultStyle is already set as #{style}"
+  print "#{name} is a vector; "
+  style = parsed_response.xpath('//defaultStyle').text.strip
+
+  expected_style = expected_styles[druid] || 'generic'
+
+  if style != expected_style && (style.empty? || style == 'generic')
+    print " setting style (current: '#{style}') to '#{expected_styles[druid]}'"
+    generic_style =
+      <<~XML
+        <layer>
+          <defaultStyle>
+            <name>#{expected_styles[druid]}</name>
+          </defaultStyle>
+        </layer>
+      XML
+    update = conn.put do |req|
+      req.url layer_rest.gsub(geoserver_url, '')
+      req.headers['Content-Type'] = 'text/xml'
+      req.body = generic_style
     end
+    puts update.status
+  else
+    puts " defaultStyle is already set as #{style}"
   end
 end
