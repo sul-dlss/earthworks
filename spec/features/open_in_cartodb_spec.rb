799df4ca0f49e2b583fd0910927acbd1c603d41a
Jack Reed <phillipjreed@gmail.com>
2020-08-07 07:08:54 -0600

    Update GeoBlacklight to latest coming from master preparing for v3.0

--- spec/features/open_in_cartodb_spec.rb
+++ spec/features/open_in_cartodb_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 feature 'Open in CartoDB' do
   scenario 'item is available' do
     visit solr_document_path'stanford-cz128vq0535'
-    expect(page).to have_css 'li.exports a'
+    expect(page).to have_css 'li.carto a'
   end
   scenario 'item is not available' do
     visit solr_document_path'harvard-ntadcd106'
-    expect(page).to_not have_css 'li.exports a'
+    expect(page).to_not have_css 'li.carto a'
   end
 end
