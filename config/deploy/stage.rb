1f1379e50e9a05027fbbef5e2e2f370940245639
Chris Beer <cabeer@stanford.edu>
2022-09-06 10:20:06 -0700

    Use new earthworks servers

--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -1,7 +1,8 @@
 set :bundle_without, %w[test development deployment].join(' ')
 
-server 'kurma-earthworks-stage.stanford.edu', user: 'geostaff', roles: %w[web db app]
-server 'kurma-earthworks-worker-stage-a.stanford.edu', user: 'geostaff', roles: %w[app background]
+server 'earthworks-stage-a.stanford.edu', user: 'geostaff', roles: %w[web db app]
+server 'earthworks-stage-b.stanford.edu', user: 'geostaff', roles: %w[web db app]
+server 'earthworks-worker-stage-a.stanford.edu', user: 'geostaff', roles: %w[app background]
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
