04785e1607dde4a4114a137bf28078bf8aa27fc0
Chris Beer <chris@cbeer.info>
2022-09-12 12:56:52 -0700

    Update prod hosts

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -2,9 +2,9 @@
 
 set :bundle_without, %w[test development deployment].join(' ')
 
-server 'kurma-earthworks1-prod.stanford.edu', user: 'geostaff', roles: %w[web db app]
-server 'kurma-earthworks2-prod.stanford.edu', user: 'geostaff', roles: %w[web db app]
-server 'kurma-earthworks-worker-prod-a.stanford.edu', user: 'geostaff', roles: %w[app background whenevs]
+server 'earthworks-prod-a.stanford.edu', user: 'geostaff', roles: %w[web db app]
+server 'earthworks-prod-b.stanford.edu', user: 'geostaff', roles: %w[web db app]
+server 'earthworks-worker-prod-a.stanford.edu', user: 'geostaff', roles: %w[app background whenevs]
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
