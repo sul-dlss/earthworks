7e1a2917960635d7f738aa2cbf81a7ae09d1fe0f
Nick Budak <budak@stanford.edu>
2023-04-10 12:44:41 -0700

    Remove deployment config for UAT worker box
    The only task that runs on the worker is the CheckLayerJob that
    uses GeoMonitor to check layer availability; we don't need to
    have both prod and UAT running this job since they're checking
    the same set of layers and updating the same solr core.
    
    Ref #942

--- config/deploy/uat.rb
+++ config/deploy/uat.rb
@@ -4,9 +4,6 @@ set :bundle_without, %w[test development deployment].join(' ')
 
 server 'earthworks-uat-a.stanford.edu', user: 'geostaff', roles: %w[web db app]
 server 'earthworks-uat-b.stanford.edu', user: 'geostaff', roles: %w[web db app]
-server 'earthworks-worker-uat-a.stanford.edu', user: 'geostaff', roles: %w[app background whenevs]
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
-
-set :sidekiq_roles, :background
