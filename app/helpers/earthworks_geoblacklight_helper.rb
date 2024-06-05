a55ef14ebab906e10d11d0707b6d91d3af53ecb3
Nick Budak <budak@stanford.edu>
2024-06-05 13:19:40 -0700

    Render the description as separate paragraphs

--- app/helpers/earthworks_geoblacklight_helper.rb
+++ app/helpers/earthworks_geoblacklight_helper.rb
@@ -5,4 +5,11 @@ module EarthworksGeoblacklightHelper
     (@document.public? && @document.available?) ||
       (@document.same_institution? && user_signed_in? && @document.available?)
   end
+
+  # Override to render multi-valued description as individual paragraphs
+  def render_value_as_truncate_abstract(args)
+    tag.div class: 'truncate-abstract' do
+      Array(args[:value]).flatten.collect { |v| concat tag.p(v) }
+    end
+  end
 end
