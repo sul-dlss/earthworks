8d62c9246ac4e551da4e70e11cf5858f7c01b682
Nick Budak <budak@stanford.edu>
2024-07-31 15:44:47 -0700

    Add vendorized GBL JS

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
+import "@hotwired/turbo-rails";
 import "bootstrap";
 import "@github/auto-complete-element";
 import "blacklight";
-// import "controllers"
+import "controllers/application";
+import "geoblacklight";
+import "controllers";
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,28 +1,10 @@
 // Import and register all your controllers from the importmap under controllers/*
 
-import { application } from "controllers/application"
-import {
-  CloverViewerController,
-  LeafletViewerController,
-  OpenlayersViewerController,
-  OembedViewerController,
-  SearchResultsController,
-  DownloadsController,
-  ClipboardController,
-} from "@geoblacklight/frontend";
-
-// Register GeoBlacklight stimulus controllers
-application.register("clover-viewer", CloverViewerController);
-application.register("leaflet-viewer", LeafletViewerController);
-application.register("oembed-viewer", OembedViewerController);
-application.register("openlayers-viewer", OpenlayersViewerController);
-application.register("search-results", SearchResultsController);
-application.register("downloads", DownloadsController);
-application.register("clipboard", ClipboardController);
+import { application } from "controllers/application";
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
+eagerLoadControllersFrom("controllers", application);
 
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
