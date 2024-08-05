cf29761cac7f30bdac95770a9ac1738c404cba1c
dnoneill <noneill@stanford.edu>
2024-08-05 13:29:34 -0400

    fix tests

--- spec/features/top_navbar_spec.rb
+++ spec/features/top_navbar_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe 'Top Navbar' do
   it 'has links' do
     visit root_path
-    within '#topnav' do
-      expect(page).to have_link 'Stanford University Libraries'
+    within 'header' do
+      expect(page).to have_link 'Stanford University'
       expect(page).to have_link 'Login'
       expect(page).to have_link 'Feedback'
     end
