9120ad5c6d65182b8fcccdb22cac9620d970021b
Peter Mangiafico <peter@mangiafico.org>
2024-08-22 13:20:10 -0700

    also method to component

--- app/components/earthworks/also_available_component.rb
+++ app/components/earthworks/also_available_component.rb
@@ -10,6 +10,11 @@ module Earthworks
       super
     end
 
+    # a list of all the available other urls for the document
+    def also_available_links
+      { SearchWorks: document.searchworks_url }.compact_blank
+    end
+
     def render?
       document.also_available_links.present?
     end
