080af64bfda3c8a3820cc30da88b9637b0891322
mlongley <mlongley@stanford.edu>
2024-08-22 11:52:03 -0400

    Use Stimulus targets for expand collapse button styles

--- app/javascript/controllers/expandcollapse_controller.js
+++ app/javascript/controllers/expandcollapse_controller.js
@@ -1,16 +1,29 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static outlets = [ "description" ]
-  toggleDescriptions(evt) {
-    const activeClass = 'active'
-    const currentActive = this.element.querySelector(`.${activeClass}`)
-    currentActive.classList.remove(activeClass)
-    currentActive.disabled = false;
-    evt.currentTarget.disabled = true;
-    evt.currentTarget.classList.add(activeClass)
-    this.descriptionOutletElements.forEach(function(element) {
-      element.classList.toggle("collapse");
+  static outlets = ["description"]
+  static targets = ["expandButton", "collapseButton"]
+
+  expandDescriptions() {
+    this.descriptionOutlets.forEach(description => {
+      description.expand();
     });
+
+    this.toggleButtonStates();
   }
+
+
+  collapseDescriptions() {
+    this.descriptionOutlets.forEach(description => {
+      description.collapse();
+    });
+
+    this.toggleButtonStates();
+  }
+
+  toggleButtonStates() {
+    this.expandButtonTarget.classList.toggle("active");
+    this.collapseButtonTarget.classList.toggle("active");
+  }
+
 }
