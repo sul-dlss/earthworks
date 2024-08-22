d386a0d61685cd2cd7459c62dbec40073bdf9d18
mlongley <mlongley@stanford.edu>
2024-08-22 11:52:03 -0400

    Use Stimulus targets for expand collapse button styles

--- app/javascript/controllers/description_controller.js
+++ app/javascript/controllers/description_controller.js
@@ -1,4 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+    expand() {
+        this.element.classList.remove("collapse");
+    }
+
+    collapse() {
+        this.element.classList.add("collapse");
+    }
 }
