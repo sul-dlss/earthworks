424cdea009f399b56b52891b4b8addecd697b394
Peter Mangiafico <peter@mangiafico.org>
2024-08-26 09:31:00 -0700

    add code comment

--- app/components/earthworks/system/dropdown_component.rb
+++ app/components/earthworks/system/dropdown_component.rb
@@ -4,6 +4,8 @@ module Earthworks
   module System
     class DropdownComponent < Blacklight::System::DropdownComponent
      def before_render
+        # this is overriding the default button classes set in Blacklight
+        # (we want to use btn-outline-primary for Earthworks instead of btn-outline-secondary)
         with_button(label: button_label, classes: %w[btn btn-outline-primary dropdown-toggle])
         super
       end
