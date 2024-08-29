c0341133f43d5ce22b6a84ba24b88862707e292c
dnoneill <noneill@stanford.edu>
2024-08-29 14:40:02 -0400

    move title to span metadata and map

--- app/components/earthworks/document_component.rb
+++ app/components/earthworks/document_component.rb
@@ -2,5 +2,14 @@
 
 module Earthworks
   class DocumentComponent < Geoblacklight::DocumentComponent
+    def classes
+      [
+        @classes,
+        helpers.render_document_class(@document),
+        'document',
+        'col-lg-6',
+        ("document-position-#{@counter}" if @counter)
+      ].compact.flatten
+    end
   end
 end
