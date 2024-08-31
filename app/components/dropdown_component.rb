d156dbbb01299be85e82435ebb1169f47927ef07
Peter Mangiafico <pmangiafico@stanford.edu>
2024-08-31 16:27:02 -0400

    1242 override dropdown button (#1348)
    * override dropdown button to add a new different class
    
    * using configurable dropdowncomponent to change class of button

---
+++ app/components/dropdown_component.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class DropdownComponent < Blacklight::System::DropdownComponent
+  def before_render
+    # this is overriding the default button classes set in Blacklight
+    # (we want to use btn-outline-primary for Earthworks instead of btn-outline-secondary)
+    with_button(label: button_label, classes: %w[btn btn-outline-primary dropdown-toggle])
+    super
+  end
+end
