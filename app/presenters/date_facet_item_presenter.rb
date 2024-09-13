68d9262dff16c56844f65fa1de27f5f2e027de3c
Nick Budak <budak@stanford.edu>
2024-09-13 13:43:39 -0700

    Trim leading zeroes when presenting hierarchical years

--- app/presenters/date_facet_item_presenter.rb
+++ app/presenters/date_facet_item_presenter.rb
@@ -2,8 +2,13 @@
 
 # Format dates for display
 class DateFacetItemPresenter < Blacklight::FacetItemPivotPresenter
+  # Choose the finest granularity available and trim leading zeroes
+  # 0100-0199 -> 100-199
   def label
     century, decade, year = value.split(':')
-    [year, decade, century].find(&:present?) # label is just the finest level of granularity available
+    finest = [year, decade, century].find(&:present?)
+    finest.sub!(/^0+(\d+)/, '\1')
+    finest.sub!(/-0+(\d+)/, '-\1')
+    finest
   end
 end
