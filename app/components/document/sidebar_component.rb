5fb83a549330d972c4fb451b6f0a45158b736843
Nick Budak <budak@stanford.edu>
2024-10-31 13:26:35 -0700

    Refactor sidebar component and combine secondary portions
    This refactors the sidebar component to make it easier to determine
    what is being rendered in each of the two sections and to render
    it without accidentally duplicating the divider elements.
    
    It also ensures that items that should be grouped together in the
    second section are displayed in a single box, as noted in #1437.
    
    Fixes #1437

--- app/components/document/sidebar_component.rb
+++ app/components/document/sidebar_component.rb
@@ -3,12 +3,33 @@
 # Render the sidebar on the show view
 module Document
   class SidebarComponent < Geoblacklight::Document::SidebarComponent
+    # Primary section of the sidebar that displays at the top
     def components
       [
         Geoblacklight::LoginLinkComponent.new(document:),
         Geoblacklight::StaticMapComponent.new(document:),
-        DownloadLinksComponent.new(document:)
+        DownloadLinksComponent.new(document:),
+        Blacklight::Document::ShowToolsComponent.new(document:)
       ]
+    end
+
+    # Second section of the sidebar that displays below the main one
+    def secondary_components
+      [
+        AlsoAvailableComponent.new(document:),
+        'relations_container',
+        Blacklight::Document::MoreLikeThisComponent.new(document:)
+      ]
+    end
+
+    # Determine if there's actually anything to show by rendering everything
+    def filter_components(components)
+      components.map { |component| render component, document: }.compact_blank
+    end
+
+    # Rendered in between each component
+    def separator
+      content_tag :hr, nil, style: 'border-top: dotted 1px;'
     end
   end
 end
