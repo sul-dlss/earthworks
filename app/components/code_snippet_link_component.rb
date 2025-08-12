db77debdedde6c7819a27f1499ecb72abc690811
Nick Budak <budak@stanford.edu>
2025-08-12 10:19:42 -0700

    Make CodeSnippetLinkComponent inherit from ActionComponent
    The things in the show tools partial now get passed two kwargs
    that ViewComponent::Base was not ready for, so make this guy
    inherit from Blacklight's ActionComponent instead to accept them.

--- app/components/code_snippet_link_component.rb
+++ app/components/code_snippet_link_component.rb
@@ -1,5 +1,5 @@
 # Display link that triggers modal with code snippet content
-class CodeSnippetLinkComponent < ViewComponent::Base
+class CodeSnippetLinkComponent < Blacklight::Document::ActionComponent
   # Do not display the link to code snippets if the document is restricted
   def key
     'code_snippet_link'
