43d669b7af23e772545234386f63793dc5539efc
Nick Budak <budak@stanford.edu>
2025-08-25 09:21:25 -0700

    Update indexing-related configuration

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -2,7 +2,7 @@ set :bundle_without, %w[test development deployment].join(' ')
 
 server 'earthworks-prod-a.stanford.edu', user: 'geostaff', roles: %w[web db app]
 server 'earthworks-prod-b.stanford.edu', user: 'geostaff', roles: %w[web db app]
-server 'earthworks-worker-prod-a.stanford.edu', user: 'geostaff', roles: %w[app background cron]
+server 'earthworks-worker-prod-a.stanford.edu', user: 'geostaff', roles: %w[app background cron indexer]
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
