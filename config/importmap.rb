66156f7b2fe9901321b16dff2df624ab4b2962a5
dnoneill <noneill@stanford.edu>
2026-01-13 16:53:37 -0500

    Update auto-complete-element CDN link

--- config/importmap.rb
+++ config/importmap.rb
@@ -11,7 +11,7 @@ pin 'bootstrap', to: 'https://cdn.skypack.dev/bootstrap@5.3.3'
 
 # Blacklight
 pin 'blacklight', to: 'https://cdn.skypack.dev/blacklight-frontend@8.4.0/dist/blacklight.js'
-pin '@github/auto-complete-element', to: 'https://cdn.skypack.dev/@github/auto-complete-element'
+pin '@github/auto-complete-element', to: 'https://cdn.jsdelivr.net/npm/@github/auto-complete-element@3.8.0/+esm'
 
 # EarthWorks
 pin_all_from 'app/javascript/controllers', under: 'controllers'
