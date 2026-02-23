466f6ca443b988975274fa56440e23fb1ce902f2
Nick Budak <budak@stanford.edu>
2026-02-23 11:01:01 -0800

    We actually do have a record that matches 'xyz'
    Use something that will cause no results.

--- spec/features/zero_results_spec.rb
+++ spec/features/zero_results_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'Zero results' do
   it 'includes feedback text and link' do
-    visit search_catalog_path q: 'xyz'
+    visit search_catalog_path q: 'noresultsXXXX'
     within '.noresults' do
       expect(page).to have_css '.ask-for-dataset'
       expect(page).to have_link 'feedback'
