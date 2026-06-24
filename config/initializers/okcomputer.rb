fd1c99522afd54944f96451031108e6e2e98c8d4
Justin Coyne <jcoyne@justincoyne.com>
2026-06-24 16:16:58 -0500

    Fix rubocop squwaks in okcomputer

--- config/initializers/okcomputer.rb
+++ config/initializers/okcomputer.rb
@@ -1,15 +1,13 @@
 # override the default behavior so we can override the root engine path to run all checks
 OkComputer.mount_at = false
 
-# rubocop:disable Layout/ArgumentAlignment
-OkComputer::Registry.register 'solr',
-  OkComputer::HttpCheck.new(Blacklight.connection_config[:url].to_s.sub(%r{/$}, '') + '/admin/ping')
+ping_url = "#{Blacklight.connection_config[:url].to_s.sub(%r{/$}, '')}/admin/ping"
+OkComputer::Registry.register 'solr', OkComputer::HttpCheck.new(ping_url)
 
 OkComputer::Registry.register 'downloads-cache',
-  OkComputer::DirectoryCheck.new(Settings.DOWNLOAD_PATH, true)
+                              OkComputer::DirectoryCheck.new(Settings.DOWNLOAD_PATH, true)
 
 OkComputer::Registry.register 'redis',
-  OkComputer::RedisCheck.new(url: ENV.fetch('REDIS_URL') { Settings.redis_url })
-# rubocop:enable Layout/ArgumentAlignment
+                              OkComputer::RedisCheck.new(url: ENV.fetch('REDIS_URL') { Settings.redis_url })
 
 OkComputer.make_optional %w[redis]
