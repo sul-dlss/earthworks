33c3a1391f82fac59abe2e3e24f31919213a573e
Nick Budak <budak@stanford.edu>
2026-07-17 07:57:41 -0700

    Make the RecentlyAddedComponent link to a search sorted that way
    Now that we have a sort option for recently updated/modified, we
    can make the links on the homepage go to a search that is also
    sorted that way, which matches the items shown.

--- spec/components/recently_added_list_component_spec.rb
+++ spec/components/recently_added_list_component_spec.rb
@@ -10,4 +10,11 @@ RSpec.describe RecentlyAddedListComponent, type: :component do
   it 'renders the correct number of items by default' do
     expect(rendered_component).to have_css('li', count: 4)
   end
+
+  it 'renders a link to the search results page' do
+    expect(rendered_component).to have_link(
+      'Browse all datasets',
+      href: '/catalog?f%5Bgbl_resourceClass_sm%5D%5B%5D=Datasets&sort=gbl_mdModified_dt+desc'
+    )
+  end
 end
