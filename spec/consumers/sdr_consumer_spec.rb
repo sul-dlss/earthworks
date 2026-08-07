d675553adc9994d7722a6a124305c98267d551a2
Nick Budak <budak@stanford.edu>
2026-08-07 12:02:46 -0700

    Don't skip items with catkeys and remove the option
    This makes sense for items going to Searchworks, where you
    might double-index from FOLIO and SDR, but it'll never happen
    for data going to Earthworks because the FOLIO indexer doesn't
    target Earthworks.

--- spec/consumers/sdr_consumer_spec.rb
+++ spec/consumers/sdr_consumer_spec.rb
@@ -96,42 +96,6 @@ RSpec.describe SdrConsumer do
       end
     end
 
-    context 'when the record has a catkey' do
-      let(:record) { instance_double(CocinaDisplay::CocinaRecord, folio_hrid: 'a12345') }
-
-      it 'executes a delete' do
-        expect(solr_service).to receive(:delete_by_ids).with(['abc123'])
-        consumer.process_batch([message])
-      end
-    end
-
-    context 'when skip_catkey is false and the record has a catkey' do
-      subject(:consumer) do
-        described_class.new(
-          target: 'earthworks',
-          skip_catkey: false,
-          cocina_service: cocina_service,
-          solr_service: solr_service
-        )
-      end
-
-      let(:record) { instance_double(CocinaDisplay::CocinaRecord, folio_hrid: 'a12345') }
-
-      it 'executes an update' do
-        expect(solr_service).to receive(:update).with(record)
-        consumer.process_batch([message])
-      end
-    end
-
-    context 'when the item is deleted' do
-      let(:message_contents) { { druid: 'druid:abc123', deleted_at: '2024-06-02T00:00:00Z' } }
-
-      it 'executes a delete' do
-        expect(solr_service).to receive(:delete_by_ids).with(['abc123'])
-        consumer.process_batch([message])
-      end
-    end
-
     context 'with multiple deletes' do
       it 'deletes them in one Solr request' do
         messages = %w[abc123 def456 ghi789].map do |druid|
