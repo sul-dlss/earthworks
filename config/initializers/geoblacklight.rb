20b14bfd94ed8e101970709213a4a10df87f68b0
Nick Budak <budak@stanford.edu>
2026-09-18 10:29:49 -0700

    Set up configuration to supply a CARTO API key for basemaps
    CARTO evidently hasn't wired up API key sending for the vector
    basemaps yet, so we can't turn it on until they do, but now
    the config is there for when the watermarks appear again.

--- config/initializers/geoblacklight.rb
+++ config/initializers/geoblacklight.rb
@@ -1,10 +1,25 @@
-# Nothing left to configure here after the GeoBlacklight 6 viewer rework, but the
-# two things this file used to set are worth a pointer:
+# Configuration for GeoBlacklight; see:
+# https://github.com/geoblacklight/geoblacklight/blob/main/lib/geoblacklight/configuration.rb
+config = Geoblacklight.configuration
+
+# Supply our CARTO API key so basemaps don't get watermarked. See:
+# https://docs.carto.com/faqs/carto-basemaps
 #
-# * Map colours moved from Ruby (config.leaflet_options) to the --ogm-* CSS custom
-#   properties that ogm-viewer reads; ours are in app/assets/stylesheets/earthworks.css.
-#   Basemaps are now light_basemap_url / dark_basemap_url, each a URL to a MapLibre
-#   style document, and are left unset so the viewer uses its own defaults.
+# It's OK for this to be public on the client side because it's restricted to
+# Stanford origins (*.stanford.edu) in our CARTO configuration.
+CARTO_API_KEY = 'cb1_3pzn_1_1af10a23e7b04a57f0e9e5f4'.freeze
+
+# We use 'positron' for light basemaps and 'darkMatter' for dark ones; this
+# is the same defaults as ogm-viewer, just with an API key appended.
 #
-# * Per-relationship icons are gone. Related records are labelled with a
-#   resource-class badge (Geoblacklight::HeaderBadgesComponent) instead.
+# These URLs must point to MapLibre JSON style documents. For CARTO basemaps,
+# the API key gets appended; for others it's not required.
+#
+# For now, appending the key to the vector basemaps actually 403s, because CARTO
+# apparently hasn't turned it on yet. When the watermark shows up again, swap
+# these for the versions below that send the API key.
+config.light_basemap_url = 'https://basemaps.cartocdn.com/gl/positron-gl-style/style.json'
+config.dark_basemap_url = 'https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json'
+#
+# config.light_basemap_url = "https://basemaps.cartocdn.com/gl/positron-gl-style/style.json?key=#{CARTO_API_KEY}"
+# config.dark_basemap_url = "https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json?key=#{CARTO_API_KEY}"
